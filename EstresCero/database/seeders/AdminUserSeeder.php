d1d7b409f1a460936864b170a41ac9a327f582bc
Kevin15032 <kdtorres1503@gmail.com>
2025-08-04 00:36:26 -0600

    usuario admin

--- EstresCero/database/seeders/AdminUserSeeder.php
+++ EstresCero/database/seeders/AdminUserSeeder.php
@@ -2,24 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::create([
-            'name' => 'Kevin Dominguez',
-            'email' => 'domingueztorreskevin@gmail.com',
-            'password' => Hash::make('T15030315'),
-            'is_admin' => true,
-            'is_active' => true,
-        ]);
+        User::updateOrCreate(
+            ['email' => 'domingueztorreskevin@gmail.com'],
+            [
+                'name' => 'Kevin Dominguez',
+                'password' => Hash::make('T15030315'), // Puedes cambiar la contraseña si deseas
+                'is_admin' => true,
+                'is_active' => true,
+                'email_verified_at' => now(), // Para evitar pedir confirmación por correo
+            ]
+        );
     }
 }
+
